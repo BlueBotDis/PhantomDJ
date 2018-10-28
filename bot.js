d706e8a9a0000fc3215648e57adc5edfc83fda94
BlueBotDis <44535227+BlueBotDis@users.noreply.github.com>
2018-10-28 21:02:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -469,26 +469,50 @@ function play(guild, song) {
     }).then(message =>{message.delete(5000)})
 }
  
+const developers = ["321121048403771393","427855446225846272"]
 client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "321121048403771393") return;
- 
-if (message.content.startsWith(PREFIX + 'setstream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
-}
- 
-if (message.content.startsWith(PREFIX + 'setname')) {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'sp')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'sw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-}
-if (message.content.startsWith(PREFIX + 'setavatar')) {
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+client.on("message", async message => {
+if(message.content.startsWith("Mem")) {
+let args = message.content.split(" ").slice(1).join(" ")
+if(message.author.id != "490927770143031296") return;
+if(!args) return message.reply("._.");
+message.delete();
+var emb = new  Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setDescription(args);
+message.channel.send(emb)
 }
 });
 client.login(process.env.BOT_TOKEN);
